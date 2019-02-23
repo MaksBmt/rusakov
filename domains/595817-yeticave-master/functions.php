cc43355b18146bfa79b8b9c6775853a94705d2be
MaksBmt <solov@tdpsm.ru>
2019-02-23 10:01:46 +0200

    Александр помог довести до ума шаблонизатор

--- domains/595817-yeticave-master/functions.php
+++ domains/595817-yeticave-master/functions.php
@@ -1,20 +1,24 @@
-
-
 <?php
-        
-        
-//$title__page = 'Йети-кафе';
-//$url_connection = "templates/layout.php";
-
-function connection_page($url_connection, $arr) {
-   
- $connect = include_once($url_connection);
-    
-    return $connect;
-    
-        
+/**
+ * Функция-шаблонизатор
+ *
+ * @param string $template_name Имя PHP-шаблона (без расширения)
+ * @param array $data Массив с данными для шаблона
+ * @param string $template_file Путь к файлу шаблона
+ *
+ * @return string $output Итоговый HTML-код из шаблона, где элементы $data заменены их значениями
+ */
+function include_template($template_name, $data) {
+    $template_file = 'templates/' . $template_name . '.php';
+    if (file_exists($template_file)) {
+        ob_start();
+        include($template_file);
+        $output = ob_get_clean();
+    }
+    else {
+        $output = '';
+    }
+    return $output;
 }
-        
-        ?>
         
         
--- domains/595817-yeticave-master/index.php
+++ domains/595817-yeticave-master/index.php
@@ -1,12 +1,12 @@
 <?php 
 
-include('functions.php');
-include('data.php');
+include('functions.php'); // подключаем функцию
+include('templates/data.php'); //подключаем массив, так как он в папке templates то адрес соответствующий
 
 
-
-$content = connection_page('templates/index.php', $lots);
-$layout = connection_page('templates/layout.php', $categories);
-
-print($layout);
-?>
+// получаем тело страницы (templates/index.php)
+$layout_data['content'] = include_template('index', $index_data);
+// получаем обертку страницы (templates/layout.php)
+$layout = include_template('layout', $layout_data);
+//так как у нас в файле functions.php прописаны расширения и адрес то здесь они не нужны
+print ($layout);
