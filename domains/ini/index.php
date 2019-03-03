3b4800cf7624bec0705652d46cd603031522ebe5
MaksBmt <solov@tdpsm.ru>
2019-03-03 19:59:11 +0200

    Добавлено в папку ini задание по IntlChar - преобразование по коду в символы

--- domains/ini/index.php
+++ domains/ini/index.php
@@ -16,6 +16,29 @@
 Этот текст должен быть красного цвета и большим шрифтом</p>
 	<p style="color: <?=$a_1?>; font-size: <?=$b_2?>;">А этот текст будет синим и маленького размера</p>
 	
+	<?php echo '<br>';
+   $intlcod = IntlChar::ord('N');
+   echo $intlcod;
+   echo '<br>';
+   ?>
+	<div style="margin: 10px 200px; display: flex; flex-direction: column; background-color: blue;">
+   <table style="margin: 0 auto; background-color: #FDF9F9;">
+	   <caption>Значение и Символ</caption>
+	   <tr>
+	   <td style="width: 180px; border: 1px solid #000000; text-align: center; color: red; font-size: 26px;">Символ</td>
+	   <td style="width: 180px; border: 1px solid #000000; text-align: center; color: red; font-size: 26px;">Значение</td>
+		   <?php for($i=0; $i<40; $i++): ?>
+		   <tr>
+			   <td style=" border: 1px solid #000000; text-align: center; color: #083BA2; font-size: 18px;"><?php echo $i; ?></td>
+			   <td style=" border: 1px solid #000000; text-align: center; color: #083BA2; font-size: 18px;"><?php echo (Intlchar :: chr($i)); ?></td>
+	   </tr>
+	   <?php endfor; ?>
+
+	</table>
+		</div
+		   
+	
+	
 <body>
 </body>
 </html>
